6e74e72bd53dc25fca6f61d3305bba1a76857be6
surajraika <surajraika5sr@gmail.com>
2025-11-25 13:05:56 +0530

    FIX DATABASES Insitalisation error and reliability

--- config.php
+++ config.php
@@ -2,20 +2,26 @@
 if (basename($_SERVER['PHP_SELF']) == 'config.php') {
   header("Location: index.php");
   exit;
-}
 
 define("DB_SERVER", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "password");
 define("DB_NAME", "registered");
 
-# Connection
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-# Check connection
-if (!$link) {
-  // die("Connection failed: " . mysqli_connect_error());
-  header("Location: index.php");
+
+
+try {  # Connection
+  $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+} catch(mysqli_sql_exception $e){
+  header("Location: service-down.php");
   exit;
+}
 
+
+
+
+if (!$link){
+  header("Location: service-down.php");
+  exit;
 }
